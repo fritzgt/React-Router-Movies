c5571507472fd9802f18b0267353fa10ed3daada
fritzgt <tuanis@live.com>
2019-06-05 21:22:41 -0400

    Completed wrapping the App with router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
+
+import { Route } from "react-router-dom";
 
 export default class App extends Component {
   constructor() {
@@ -23,6 +25,8 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <div>Replace this Div with your Routes</div>
+        <Route path="/" component="MovieList" exact />
+        <Route path="/movies/:id" component="Movie" />
       </div>
     );
   }
